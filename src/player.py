6f0d22c60c3854ce89e32282f2c73f05d97f94cf
Jordan Esperance <jordan.esperance@yahoo.com>
2025-08-12 15:14:21 -0400

    standard push

--- src/player.py
+++ src/player.py
@@ -7,6 +7,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
 
+
         original_image = pygame.image.load("/Users/jordanesperance/PycharmProjects/ArboriaAttempt1/.venv/Sprites/hero.png").convert_alpha()
         self.image = pygame.transform.scale(original_image, (64, 64))
         self.rect = self.image.get_rect(topleft=pos)
